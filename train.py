4a7290dc4e0c792125e83d1f72fa2366f9c27158
VietAnh-1999 <nguyenvietanhtdh4@gmail.com>
2025-05-19 14:14:52 +0700

    V1 1905

---
+++ train.py
@@ -0,0 +1,25 @@
+# code_train
+
+import torch
+from ultralytics import YOLO
+import wandb
+
+# Đăng nhập vào Weights & Biases (W&B) để theo dõi quá trình huấn luyện (tuỳ chọn)
+#wandb.login(key="38f8ad7ec777cb01a116465827f9f72f7207f22")
+
+# Tải mô hình YOLOv8n (lightweight)
+#model = YOLO("yolov8n.pt")
+# Nếu muốn tiếp tục huấn luyện từ lần huấn luyện trước:
+model = YOLO("last.pt")
+
+# Bắt đầu huấn luyện
+model.train(
+    data= r"D:\12.python\HT\XLAMAU\data.yaml",
+    epochs=50,
+    imgsz=640,
+    batch=16,
+    project="my-yolo-project",
+    name="yolov8n-custom"
+)
+
+
