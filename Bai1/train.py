7610f26227e6d8631dceb452528a9552d53e983c
VietAnh-1999 <nguyenvietanhtdh4@gmail.com>
2025-05-21 10:44:10 +0700

    2105

--- Bai1/train.py
+++ Bai1/train.py
@@ -9,17 +9,17 @@ import wandb
 #wandb.login(key="38f8ad7ec777cb01a116465827f9f72f7207f22")
 
 # Tải mô hình YOLOv8n (lightweight)
-# model = YOLO("yolov8n.pt")
+model = YOLO("yolov8s.pt")
 # Nếu muốn tiếp tục huấn luyện từ lần huấn luyện trước:
-model = YOLO("last.pt")
+#model = YOLO("last.pt")
 
 # Bắt đầu huấn luyện
 model.train(
     data= "data.yaml",
     epochs=100,
     imgsz=640,
-    batch=20,
+    batch=16,
     project="my-yolo-project",
-    name="yolov8n-custom"
+    name="yolov8-custom"
 )
 
